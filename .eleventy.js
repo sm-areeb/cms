649e882ce7b20470c1b54770b846cf9cfb563aa7
sm-areeb <smareeb@ymail.com>
2023-09-23 17:35:31 +0530

    header footer

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,11 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addLayoutAlias('base', 'layouts/baseLayout.11ty.js');
     eleventyConfig.addPassthroughCopy({
       "./src/admin/*": "./admin/",
-      "./node_modules/bulma/css/bulma.min.css":
-        "./static/css/bulma.min.css",
+      "./src/css/": "./static/css",
+      "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js":
+        "./static/js/bootstrap.bundle.min.js",
+      "./node_modules/bootstrap/dist/css/bootstrap.min.css":
+          "./static/css/bootstrap.min.css",
     });
     eleventyConfig.addPassthroughCopy("images");
     return {
--- src/_includes/partials/header.11ty.js
+++ src/_includes/partials/header.11ty.js
@@ -1,5 +1,29 @@
-module.exports = function(data) {
-    return /*html*/`
-        <nav>header ${data?.title || ''}</nav>
+module.exports = function (data) {
+  return /*html*/ `<div class="navbar-wrapper">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="/images/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+                Bootstrap
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
     `;
 };
