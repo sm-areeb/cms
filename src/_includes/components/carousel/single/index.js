44efd5caafd0f7b392df07f00f5d6868e5b08045
sm-areeb <smareeb@ymail.com>
2023-10-10 16:54:00 +0530

    fix new lines

--- src/_includes/components/carousel/single/index.js
+++ src/_includes/components/carousel/single/index.js
@@ -6,12 +6,10 @@ function SingleCarousel(props) {
             ${
               header &&
               /* html */ `<div className="carousel-header"><h1>${header}</h1></div>`
-            }
-            ${
+            }${
               description &&
               /* html */ `<div className="carousel-description"><p>${description}</p></div>`
-            }
-            <ol class="carousel-indicators">${gallery.map((item, index) => /*html*/`<li data-target="#${uuid}" data-slide-to="${index}" ${(index == 0) ? 'class="active"' : ''}></li>`)}
+            }<ol class="carousel-indicators">${gallery.map((item, index) => /*html*/`<li data-target="#${uuid}" data-slide-to="${index}" ${(index == 0) ? 'class="active"' : ''}></li>`)}
             </ol>
             <div class="carousel-inner">${gallery.map(
                   ({
@@ -35,13 +33,10 @@ function SingleCarousel(props) {
                                 }${
                               linkText &&
                               /*html*/ `<a href="${linkHref}" class="carousel-link ${ctaButton && 'btn btn-primary'}">Primary link</a>`
-                            }
-                                </div>`
-                            }
-                    </div>
+                            }</div>`
+                            }</div>
                 `
-                )}
-            </div>
+                )}</div>
             <a class="carousel-control-prev" href="#${uuid}" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
